22e2fbece731e6cfd0fd4080a20d09456405489d
cgs <cgs056@163.com>
2018-11-29 15:59:13 +0800

    init client

---
+++ config-client/src/main/java/com/example/clicent/ConfigClicentApplication.java
@@ -0,0 +1,12 @@
+package com.example.clicent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClicentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClicentApplication.class, args);
+	}
+}
